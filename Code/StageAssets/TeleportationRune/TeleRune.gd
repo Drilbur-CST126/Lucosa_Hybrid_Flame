d620d82f3cf52f84500a1ab921360a614add3a06
Jordan Clark <jordan.clark@oit.edu>
2021-03-07 14:48:31 -0800

    Final touches for the Winter presentation

--- Code/StageAssets/TeleportationRune/TeleRune.gd
+++ Code/StageAssets/TeleportationRune/TeleRune.gd
@@ -14,11 +14,8 @@ func set_active(val: bool):
 				get_parent().get_node(rune_name(sequence - 1)).active = true
 			GlobalData.flags.append(active_name())
 		
-		if get_node_or_null("ColorRect") != null:
-			if active:
-				$ColorRect.color = Color.white
-			else:
-				$ColorRect.color = Color.gray
+		if get_node_or_null("AnimationPlayer") != null && val:
+			$AnimationPlayer.play("Activate")
 
 static func rune_name(seq: int) -> String:
 	if seq == 1:
@@ -29,11 +26,20 @@ static func rune_name(seq: int) -> String:
 func _ready():
 	Utility.print_errors([
 		get_parent().connect("ready", self, "on_parent_loaded"),
+		$AnimationPlayer.connect("animation_finished", self, "on_anim_finished")
 	])
 	set_active(GlobalData.flags.has(active_name()))
+	if active:
+		$AnimationPlayer.play("ActiveIdle")
+	else:
+		$AnimationPlayer.play("Idle")
 	
 func on_parent_loaded():
 	next = get_parent().get_node_or_null(rune_name(sequence + 1))
+	
+func on_anim_finished(name: String):
+	if name == "Activate":
+		$AnimationPlayer.play("ActiveIdle")
 
 func active_name() -> String:
 	return String(get_path()) + "_active"
@@ -44,6 +50,11 @@ func player_entered(p: Node2D):
 
 func action():
 	if next != null && next.active:
+		next.get_node("Particles2D").emitting = true
 		player.global_position = next.global_position
 	elif get_parent().has_node(rune_name(1)):
-		player.global_position = get_parent().get_node(rune_name(1)).global_position
+		var node = get_parent().get_node(rune_name(1))
+		node.get_node("Particles2D").emitting = true
+		player.global_position = node.global_position
+	GlobalData.camera.global_position = player.global_position
+	$Particles2D.emitting = true
