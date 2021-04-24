e814b39c39e03f00bcea01a2a96f8f16927dc549
Jordan Clark <jordan.clark@oit.edu>
2021-04-23 22:12:06 -0700

    Waterfall complete in WaterfallRoom

--- Code/StageAssets/Waterfall/Waterfall.gd
+++ Code/StageAssets/Waterfall/Waterfall.gd
@@ -2,16 +2,30 @@ extends Node2D
 tool
 
 export(int, 1, 20) var height := 1 setget set_height
+export var showMist := true setget set_show_mist
+export var mistOffset := 0.0 setget set_mist_offset
 
 func set_height(val: int):
 	height = val
 	update_waterfall()
 	
+func set_show_mist(val: bool):
+	showMist = val
+	if has_node("Mist"):
+		$Mist.visible = val
+	
+func set_mist_offset(val: float):
+	mistOffset = val
+	if has_node("Mist"):
+		$Mist.position.y = val
+	
 func _ready():
 	update_waterfall()
 
 func update_waterfall():
-	$ColorRect.margin_top = -180 * height
+	if has_node("WaterfallSprite"):
+		$WaterfallSprite.region_rect.size.y = 1080 * height
+		$WaterfallSprite.position.y = -90 * height
 	if $Particles.get_child_count() < height: # Need to add more children
 		for i in range($Particles.get_child_count(), height):
 			var particle := $Particles/Particles2D.duplicate() as Particles2D
