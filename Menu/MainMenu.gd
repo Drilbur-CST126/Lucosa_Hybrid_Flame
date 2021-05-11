d7e4737e9ad4b43f996f926e48bc621900aad12e
Jordan Clark <jordan.clark@oit.edu>
2021-05-10 23:03:42 -0700

    Derenisa fight anims finished

--- Menu/MainMenu.gd
+++ Menu/MainMenu.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 func _ready():
 	#pass
-	GlobalData.debug = false
 	Utility.print_connect_errors(get_path(), [
 		$OptionVBoxContainer/NewGame.connect("pressed", self, "new_game"),
 		$OptionVBoxContainer/Continue.connect("pressed", self, "load_game"),
@@ -14,12 +13,15 @@ func _ready():
 const newGameScene := "res://Stages/FrogLands/JumpTutorialRoom.tscn"
 func new_game():
 	GlobalData.reset()
+	GlobalData.debug = false
 	Utility.print_errors([
 		get_tree().change_scene(newGameScene),
 	])
 	GlobalData.save_reload(newGameScene)
 	
 func load_game():
+	GlobalData.reset()
+	GlobalData.debug = false
 	var success := GlobalData.load_game()
 	if !success:
 		new_game()
